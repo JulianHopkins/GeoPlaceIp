16301a4df9ceeada2c0c7d6a760405added74fa0
kartsev@kommersant.local <kartsev@kommersant.ru>
2021-12-20 09:40:29 +0300

    Update for frontend

--- GeoPlaceIp/Program.cs
+++ GeoPlaceIp/Program.cs
@@ -1,20 +1,31 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
+builder.Services.AddControllersWithViews();
 
 builder.Services.AddTransient<GeoPlaceIp.Infras.IRepo, DataRepo>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
 new DataLoader();
 
 app.UseHttpsRedirection();
-
+app.UseStaticFiles();
+app.UseRouting();
 app.UseAuthorization();
 
 app.MapControllers();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
